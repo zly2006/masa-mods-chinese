30ce415edf7890aeb887029f836c146be7aa367f
DreamingLri <3507415455@qq.com>
2024-11-17 16:34:13 +0800

    🔥 remove unused tag in rename.py

--- rename.py
+++ rename.py
@@ -1,22 +1,10 @@
 import os
 import shutil
-import subprocess
-
-def get_git_tags():
-    try:
-        result = subprocess.run(["git", "describe", "--tags", "--abbrev=0"], capture_output=True, text=True, check=True)
-        tag = result.stdout.splitlines()
-        return tag
-    except subprocess.CalledProcessError as e:
-        print(f"Error while running git command: {e}")
-        return []
-
-tag = get_git_tags()[0]
 
 if os.path.exists('./masa-mods-chinese.zip'):
     file_path = './masa-mods-chinese.zip'
-    shutil.move(file_path, './[1.21]MASA全家桶汉化包' + '-' + tag + '.zip')
+    shutil.move(file_path, './[1.20]MASA全家桶汉化包' + '.zip')
 
 if os.path.exists('./masa-mods-chinese-neo.zip'):
     file_path = './masa-mods-chinese-neo.zip'
-    shutil.move(file_path, './[1.21]MASA全家桶汉化包-neo' + '-' + tag + '.zip')
+    shutil.move(file_path, './[1.20]MASA全家桶汉化包-neo' + '.zip')
